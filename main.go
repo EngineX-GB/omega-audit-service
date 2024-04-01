aeefd87ef834ba5943e96af9045d0190a99cfc50
EngineX-GB <uk.engine.x@gmail.com>
2024-04-01 22:28:19 +0100

    Updated URL and fixed entry model

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import "net/http"
 func main() {
 	fmt.Println("Started Omega Audit Service " + config.GetAppVersion())
 	http.HandleFunc("/api/v1/audit/add", handlers.AddAuditEntryHandler)
-	http.HandleFunc("/api/v1/audit/", handlers.FindAuditEntryHandler)
+	http.HandleFunc("/api/v1/audit", handlers.FindAuditEntryHandler)
 	err := http.ListenAndServe(":8083", nil)
 	if err != nil {
 		log.Fatal("Cannot run this service : " + err.Error())
